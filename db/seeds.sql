166f68f939d00f136dba14bf0408ce634295da6a
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-07-17 01:47:57 -0400

    added roles (work in progress)

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,6 +8,17 @@ VALUES (01, 'HR'),
 
 
 INSERT INTO roles (id, title, salary, department_id)
+VALUES /* HR */
+       (001, 'Talent Management', 20000.00, 01),
+       (002, 'Training and Development', 15000, 01),
+       (003, 'Workplace Safety', 25000, 01),
+       /* Accounting */
+       (004, 'Chief Financial Officer', 75000.00, 02),
+       (005, 'Director of Accounting', 50000.00, 02),
+       (006, 'Budgeting', 25000.00, 02),
+       /* Marketing */
+       (007, 'Chief Marketing Officer', 100000.00, 03),
+       (008, "Marketing & promotions manager", 50000.00, 03),
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
        
