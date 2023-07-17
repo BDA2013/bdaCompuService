0b637c091d1f3be002be7dfb2d69faaa808f3634
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-07-17 10:44:52 -0400

    roles added

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,7 +3,7 @@ VALUES (01, 'HR'),
        (02, 'Accounting'),
        (03, 'Marketing'),
        (04, 'Sales'),
-       (05, 'Customer Service')
+       (05, 'Customer Service'),
        (06, 'Information Technology')
 
 
@@ -18,7 +18,16 @@ VALUES /* HR */
        (006, 'Budgeting', 25000.00, 02),
        /* Marketing */
        (007, 'Chief Marketing Officer', 100000.00, 03),
-       (008, "Marketing & promotions manager", 50000.00, 03),
+       (008, 'Director of Marketing', 80000.00, 03),
+       (009, "Marketing & promotions manager", 50000.00, 03),
+       /* Sales */
+       (010, 'Director of Sales', 75000.00, 04),
+       (011, 'Sales Management', 70000.00, 04),
+       (012, 'Sales Representative', 50000.00, 04),
+       /* Information Technology */
+       (013, 'IT Technician', 90000.00, 05),
+       (014, 'Web Developer', 85000.00, 05),
+       (015, 'Computer Programmer', 85000.00, 05)
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
        
