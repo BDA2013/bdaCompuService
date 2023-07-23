7e42d67fae24466075d28074ce9715a2ecd3e7f4
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-07-23 11:26:39 -0400

    added switch statement

--- index.js
+++ index.js
@@ -15,7 +15,32 @@ function init() {
     inquirer.prompt(
         userInput
         )
-        //.then ((data) => );
+        .then ((data) => {
+            //console.log(data.options);
+            switch (data.options) {
+                case 'view all departments':
+                    //console.log(data.options);
+                    break;
+                case 'view all roles':
+                    //console.log(data.options);
+                    break;
+                case 'view all employees':
+                    //console.log(data.options);
+                    break;
+                case 'add a department':
+                    //console.log(data.options);
+                    break;
+                case 'add a role':
+                    //console.log(data.options);
+                    break;
+                case 'add an employee':
+                    //console.log(data.options);
+                    break;
+                case 'update an employee role':
+                    //console.log(data.options);
+                    break;
+            }
+        });
 }
 
 init()
